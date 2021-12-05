0565231b69b78310dd27bed5f53ff22f9675da22
kenzok8 <kenzok8@gmail.com>
2021-12-05 14:21:44 +0800

    int

--- luci-app-gpsysupgrade/luasrc/model/cbi/gpsysupgrade/api.lua
+++ luci-app-gpsysupgrade/luasrc/model/cbi/gpsysupgrade/api.lua
@@ -6,10 +6,10 @@ local util = require "luci.util"
 
 appname = "gpsysupgrade"
 curl = "/usr/bin/curl"
-curl_args = {"-skfL", "--connect-timeout 3", "--retry 3", "-m 60"}
+curl_args = {"-skfL", "--connect-timeout 3", "--retry 3"}
 wget = "/usr/bin/wget"
-wget_args = {"--no-check-certificate", "--quiet", "--timeout=100", "--tries=3"}
-command_timeout = 300
+wget_args = {"--quiet", "--connect-timeout=3", "--timeout=6", "--tries=2"}
+command_timeout = 60
 LEDE_BOARD = nil
 DISTRIB_TARGET = nil
 
